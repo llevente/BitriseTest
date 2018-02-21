8f0ff1c13b9ab17836b098f260e9d6626df24fb3
Levente Lorincz <llevente@inpossoft.com>
2018-02-21 16:54:31 +0200

    Test push 2

--- app/src/main/java/com/inpos/bitrisetest/MainActivity.java
+++ app/src/main/java/com/inpos/bitrisetest/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String s = "Testing auto build";
+        String s = "Testing auto build2";
     }
 }
