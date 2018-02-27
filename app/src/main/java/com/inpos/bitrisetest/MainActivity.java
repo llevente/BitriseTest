efc94f78303f9839cfb51f082d8e6c2fbc0d2adf
Levente Lorincz <llevente@inpossoft.com>
2018-02-27 13:19:22 +0200

    Loaded html5 game

--- app/src/main/java/com/inpos/bitrisetest/MainActivity.java
+++ app/src/main/java/com/inpos/bitrisetest/MainActivity.java
@@ -1,7 +1,11 @@
 package com.inpos.bitrisetest;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String s = "Testing auto build2";
+
+        String urlString = "https://www.delphitools.info/NickelIron/index.html";
+        WebView webView = findViewById(R.id.webView);
+        webView.clearCache(true);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+        });
+
+        CookieManager.getInstance().setCookie(urlString, "lenci=cookie123");
+        webView.loadUrl(urlString);
+
     }
 }
