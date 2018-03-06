63bfaeb7f374a790f71be17a9ce22b6e5f8b284a
Levente Lorincz <llevente@inpossoft.com>
2018-03-06 13:26:52 +0200

    Deploy test push

--- app/src/main/java/com/inpos/bitrisetest/MainActivity.java
+++ app/src/main/java/com/inpos/bitrisetest/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         WebView webView = findViewById(R.id.webView);
         webView.clearCache(true);
 
+
+
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient() {
 
